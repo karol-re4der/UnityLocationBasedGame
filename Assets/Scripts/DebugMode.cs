836041bbc64a902506216226e88197b9c9f08518
Re4derTheGlorious <karol.re4der@gmail.com>
2021-07-02 09:53:32 +0200

    Added debug console and debug mode UI

---
+++ Assets/Scripts/DebugMode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DebugMode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(Debug.isDebugBuild);
+        LogMessage("Running in debug mode");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LogMessage(string message)
+    {
+        transform.Find("Debug Text").GetComponent<TextMeshProUGUI>().text = message + "\n" + transform.Find("Debug Text").GetComponent<TextMeshProUGUI>().text;
+    }
+}
