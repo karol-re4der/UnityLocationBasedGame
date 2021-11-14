7297f3a2a2d889d1ceb6c9cdc90faf15ce94b8d4
Re4derTheGlorious <karol.re4der@gmail.com>
2021-11-14 19:57:49 +0100

    Profile Menu

---
+++ Assets/Resources/Scripts/UI/Menus/ProfileMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ProfileMenu : SubMenu
+{
+    public void Enter()
+    {
+        UserData ud = Globals.GetClientLogic().LatestUserData;
+        if (ud!=null)
+        {
+            content.transform.Find("Fields/Name Field/Right Text/").GetComponent<TextMeshProUGUI>().text = ud.Name;
+            content.transform.Find("Fields/Surname Field/Right Text/").GetComponent<TextMeshProUGUI>().text = ud.Surname;
+            content.transform.Find("Fields/Nickname Field/Right Text/").GetComponent<TextMeshProUGUI>().text = ud.Nickname;
+            content.transform.Find("Fields/Email Field/Right Text/").GetComponent<TextMeshProUGUI>().text = ud.Email;
+
+            base.Enter();
+        }
+        else
+        {
+            string message = ClientAPI.Prepare_WHOAMI(PlayerPrefs.GetString("Token", ""));
+            Globals.GetNetworkManager().SendMessageToServer("WHOAMI", message);
+            Globals.GetPrompt().ShowMessage("No user data loaded yet");
+        }
+    }
+}
