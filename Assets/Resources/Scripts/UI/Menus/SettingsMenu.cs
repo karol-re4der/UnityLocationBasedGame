98e2f993fb74a63238a1ffa1e3de52e1336a2e96
Re4derTheGlorious <karol.re4der@gmail.com>
2021-11-16 10:52:40 +0100

    Saving sound settings

--- Assets/Resources/Scripts/UI/Menus/SettingsMenu.cs
+++ Assets/Resources/Scripts/UI/Menus/SettingsMenu.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
+using TMPro;
 
 public class SettingsMenu : SubMenu
 {
+    public Button SoundButton;
+    private bool _soundMuted;
+
+    void Start()
+    {
+        _soundMuted = PlayerPrefs.GetInt("Sound", 1)==0;
+        if (_soundMuted)
+        {
+            SoundButton.GetComponentInChildren<TextMeshProUGUI>().text = "    Sound muted";
+        }
+        else
+        {
+            SoundButton.GetComponentInChildren<TextMeshProUGUI>().text = "    Mute sound";
+        }
+    }
+
     public void Button_Sound()
     {
+        if (_soundMuted)
+        {
+            _soundMuted = false;
+            SoundButton.GetComponentInChildren<TextMeshProUGUI>().text = "    Mute sound";
+            PlayerPrefs.SetInt("Sound", 1);
 
+        }
+        else
+        {
+            _soundMuted = true;
+            SoundButton.GetComponentInChildren<TextMeshProUGUI>().text = "    Sound muted";
+            PlayerPrefs.SetInt("Sound", 0);
+
+        }
     }
+
     public void Button_Exit()
     {
         Application.Quit();
     }
+
     public void Button_Logout()
     {
         base.Exit();
